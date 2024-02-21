8ac8a3b12807b4c00fa83adfeaa1aa04265800ef
Railton Oliveira da Silva <railtonoliiveira@gmail.com>
2024-02-20 20:17:58 -0400

    create feed screen component

--- App.js
+++ App.js
@@ -1,31 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { FlatList, StyleSheet, View } from 'react-native';
-import FeedPost from './src/components/FeedPost';
-import posts from './assets/data/posts.json';
-
-const post = {
-  id: 'p1',
-  createdAt: '19 m',
-  User: {
-    id: 'u1',
-    image:
-      'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/zuck.jpeg',
-    name: 'Railton Silva',
-  },
-  description:
-    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
-  image: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/1.jpg',
-  numberOfLikes: 11,
-  numberOfShares: 2,
-};
+import { StyleSheet, View } from 'react-native';
+import FeedScreen from './src/screens/FeedScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <FlatList
-        data={posts}
-        renderItem={({ item }) => <FeedPost post={item} />}
-      />
+      <FeedScreen />
       <StatusBar style="auto" />
     </View>
   );
